a2c2efb5ee0afde07a0454b74ea0f90a6f2cc661
MurhafMoussa <murhaf.moussa@hotmail.com>
2025-03-01 10:43:27 +0300

    Add Environment config

---
+++ lib/src/config/entities/core_config_entity.dart
@@ -0,0 +1,16 @@
+import 'package:coore/src/config/entities/network_config_entity.dart';
+import 'package:coore/src/config/environment_config.dart';
+import 'package:equatable/equatable.dart';
+
+class CoreConfigEntity extends Equatable {
+  final Environment currentEnvironment;
+  final NetworkConfigEntity networkConfigEntity;
+
+  const CoreConfigEntity({
+    required this.currentEnvironment,
+    required this.networkConfigEntity,
+  });
+
+  @override
+  List<Object> get props => [currentEnvironment];
+}
---
+++ lib/src/config/environment_config.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+enum Environment { development, staging, uat, production }
+
+class EnvironmentConfig {
+  /// Load the .env file and optionally merge with additional environment variables.
+  Future<void> loadEnv(Environment environment) async {
+    switch (environment) {
+      case Environment.development:
+        await dotenv.load(fileName: '.env.development');
+        break;
+      case Environment.staging:
+        await dotenv.load(fileName: '.env.staging');
+        break;
+      case Environment.uat:
+        await dotenv.load(fileName: '.env.uat');
+        break;
+      case Environment.production:
+        await dotenv.load(fileName: '.env.production');
+        break;
+    }
+  }
+
+  String getString(String key, {String? fallback}) {
+    return dotenv.get(key, fallback: fallback);
+  }
+
+  int getInt(String key, {int fallback = -1}) {
+    return dotenv.getInt(key, fallback: fallback);
+  }
+
+  bool getBool(String key, {bool fallback = false}) {
+    return dotenv.getBool(key, fallback: fallback);
+  }
+
+  double getDouble(String key, {double fallback = -1}) {
+    return dotenv.getDouble(key, fallback: fallback);
+  }
+
+  /// Get a value with optional default.
+  String? maybeGet(String key, {String? fallback}) {
+    return dotenv.maybeGet(key, fallback: fallback);
+  }
+}
