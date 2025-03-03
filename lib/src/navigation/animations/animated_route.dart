7b1fa5bab135381de5e56fc056827608bbfff382
MurhafMoussa <murhaf.moussa@hotmail.com>
2025-03-03 17:03:14 +0300

    Add fade animated route

---
+++ lib/src/navigation/animations/animated_route.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class FadePage extends CustomTransitionPage {
+  FadePage({
+    required super.child,
+    Duration? transitionDuration,
+    Duration? reverseTransitionDuration,
+  }) : super(
+          transitionDuration:
+              transitionDuration ?? const Duration(milliseconds: 300),
+          reverseTransitionDuration:
+              reverseTransitionDuration ?? const Duration(milliseconds: 300),
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        );
+}
