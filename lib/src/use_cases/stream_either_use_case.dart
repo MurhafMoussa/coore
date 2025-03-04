f408d07e9235eaa628d42f9836121d6d03b2f4a1
MurhafMoussa <murhaf.moussa@hotmail.com>
2025-03-04 21:10:50 +0300

    Modify use cases to accept any param as an input and any entity as an output

--- lib/src/use_cases/stream_either_use_case.dart
+++ lib/src/use_cases/stream_either_use_case.dart
@@ -1,13 +1,7 @@
-import 'package:coore/src/api_handler/entities/base_entity.dart';
-import 'package:coore/src/api_handler/params/base_params.dart';
 import 'package:coore/src/typedefs/core_typedefs.dart';
 import 'package:coore/src/use_cases/usecase.dart';
 
-abstract class StreamEitherUseCase<
-  Output extends BaseEntity,
-  Input extends BaseParams
->
-    extends UseCase {
+abstract class StreamEitherUseCase<Output, Input> extends UseCase {
   const StreamEitherUseCase();
 
   RepositoryStreamResponse<Output> call(Input input);
--- lib/src/use_cases/task_either_use_case.dart
+++ lib/src/use_cases/task_either_use_case.dart
@@ -1,13 +1,7 @@
-import 'package:coore/src/api_handler/entities/base_entity.dart';
-import 'package:coore/src/api_handler/params/base_params.dart';
 import 'package:coore/src/typedefs/core_typedefs.dart';
 import 'package:coore/src/use_cases/usecase.dart';
 
-abstract class TaskEitherUseCase<
-  Output extends BaseEntity,
-  Input extends BaseParams
->
-    extends UseCase {
+abstract class TaskEitherUseCase<Output, Input> extends UseCase {
   const TaskEitherUseCase();
 
   RepositoryFutureResponse<Output> call(Input input);
