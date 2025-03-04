2e909aa2c6b540074d5f7f84b3fb1f14da729eac
MurhafMoussa <murhaf.moussa@hotmail.com>
2025-03-04 22:41:49 +0300

    Modify task either usecase name

--- lib/src/use_cases/future_either_use_case.dart
+++ lib/src/use_cases/future_either_use_case.dart
@@ -1,8 +1,8 @@
 import 'package:coore/src/typedefs/core_typedefs.dart';
 import 'package:coore/src/use_cases/usecase.dart';
 
-abstract class TaskEitherUseCase<Output, Input> extends UseCase {
-  const TaskEitherUseCase();
+abstract class FutureEitherUseCase<Output, Input> extends UseCase {
+  const FutureEitherUseCase();
 
   RepositoryFutureResponse<Output> call(Input input);
 }
