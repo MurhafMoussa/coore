aeaffa7c4796890c3abe9852aaacde281bc77998
MurhafMoussa <murhaf.moussa@hotmail.com>
2025-03-05 14:06:29 +0300

    Remove safe repository call

--- lib/src/error_handling/safe_repository_call.dart
+++ lib/src/error_handling/safe_repository_call.dart
@@ -1,30 +1,30 @@
-import 'package:coore/lib.dart';
-import 'package:fpdart/fpdart.dart';
+// import 'package:coore/lib.dart';
+// import 'package:fpdart/fpdart.dart';
 
-/// Wraps a repository request with an internet connectivity check and error handling.
-///
-/// This higher‑order function accepts a callback [onConnect] that performs the repository
-/// call (returning a [RepositoryFutureResponse<T>]). It first checks for an internet
-/// connection; if there isn’t one, [onDisconnect] will perform a call if it's not null or it will
-/// returns a [NoInternetConnectionFailure].
-/// Otherwise, it executes the [onConnect] inside a try/catch block. If an exception is caught,
-/// an [UnknownFailure] is returned.
-///
-/// This pattern allows you to reuse the connectivity and error‑handling logic for all
-/// repository calls in a functional programming style.
-Future<Either<Failure, T>> safeRepositoryCall<T>({
-  required Future<Either<Failure, T>> Function() onConnect,
-  Future<Either<Failure, T>> Function()? onDisconnect,
-  String getNoInternetConnectionMessage =
-      'No internet connection, please try again',
-}) async {
-  try {
-    if (!await getIt<NetworkStatusInterface>().isConnected) {
-      if (onDisconnect != null) return await onDisconnect.call();
-      return left(NoInternetConnectionFailure(getNoInternetConnectionMessage));
-    }
-    return await onConnect();
-  } on Exception catch (_, stackTrace) {
-    return left(UnknownFailure(stackTrace));
-  }
-}
+// /// Wraps a repository request with an internet connectivity check and error handling.
+// ///
+// /// This higher‑order function accepts a callback [onConnect] that performs the repository
+// /// call (returning a [RepositoryFutureResponse<T>]). It first checks for an internet
+// /// connection; if there isn’t one, [onDisconnect] will perform a call if it's not null or it will
+// /// returns a [NoInternetConnectionFailure].
+// /// Otherwise, it executes the [onConnect] inside a try/catch block. If an exception is caught,
+// /// an [UnknownFailure] is returned.
+// ///
+// /// This pattern allows you to reuse the connectivity and error‑handling logic for all
+// /// repository calls in a functional programming style.
+// Future<Either<Failure, T>> safeRepositoryCall<T>({
+//   required Future<Either<Failure, T>> Function() onConnect,
+//   Future<Either<Failure, T>> Function()? onDisconnect,
+//   String getNoInternetConnectionMessage =
+//       'No internet connection, please try again',
+// }) async {
+//   try {
+//     if (!await getIt<NetworkStatusInterface>().isConnected) {
+//       if (onDisconnect != null) return await onDisconnect.call();
+//       return left(NoInternetConnectionFailure(getNoInternetConnectionMessage));
+//     }
+//     return await onConnect();
+//   } on Exception catch (_, stackTrace) {
+//     return left(UnknownFailure(stackTrace));
+//   }
+// }
