2bced4ce874ede8e1edd4ae5f48810af54e553ae
MurhafMoussa <murhaf.moussa@hotmail.com>
2025-03-05 21:28:07 +0300

    Remove message viewers from exports

--- lib/src/ui/ui.dart
+++ lib/src/ui/ui.dart
@@ -4,5 +4,4 @@
 export 'base_screen.dart';
 export 'constants/constants.dart';
 export 'forms/forms.dart';
-export 'message_viewers/message_viewers.dart';
 export 'value_selector/value_selector.dart';
