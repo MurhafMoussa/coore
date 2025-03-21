9cb75c7c6bb59357af01e3b1015e9b2ff9bb3254
MurhafMoussa <murhaf.moussa@hotmail.com>
2025-03-21 15:09:07 +0300

    Fix core search widget state type

--- lib/src/ui/search/core_search_cubit.dart
+++ lib/src/ui/search/core_search_cubit.dart
@@ -7,7 +7,7 @@ part 'core_search_cubit.freezed.dart';
 part 'core_search_state.dart';
 
 class CoreSearchCubit<T> extends CoreCubit<CoreSearchState<T>, List<T>> {
-  CoreSearchCubit(this.searchFunction) : super(CoreSearchState<T>.initial());
+  CoreSearchCubit(this.searchFunction) : super(CoreSearchState.initial());
   final RepositoryFutureResponse<List<T>> Function(SearchParams) searchFunction;
   Timer? _debounceTimer;
 
@@ -37,14 +37,13 @@ class CoreSearchCubit<T> extends CoreCubit<CoreSearchState<T>, List<T>> {
   }
 
   @override
-  ApiState<List<T>> getApiState(CoreSearchState<T> state) =>
-      state.apiState as ApiState<List<T>>;
+  ApiState<List<T>> getApiState(CoreSearchState<T> state) => state.apiState;
 
   @override
   CoreSearchState<T> setApiState(
     CoreSearchState<T> state,
     ApiState<List<T>> apiState,
-  ) => state.copyWith(apiState: apiState) as CoreSearchState<T>;
+  ) => state.copyWith(apiState: apiState);
 }
 
 class SearchParams extends BaseParams {
