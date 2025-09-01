e538fb6ad19a4c221c2d1b914b6a95128eeb28a5
MurhafMoussa <murhaf.moussa@hotmail.com>
2025-09-01 03:50:11 +0300

    feat(localization): Add listener to listen for locale changes

--- lib/src/localization/widget/localization_wrapper.dart
+++ lib/src/localization/widget/localization_wrapper.dart
@@ -6,10 +6,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 /// A wrapper widget that provides [LocalizationCubit] to the widget tree
 /// and builds a [MaterialApp] whose locale is controlled by the cubit's state.
 class LocalizationWrapper extends StatelessWidget {
-  const LocalizationWrapper({super.key, required this.builder});
+  const LocalizationWrapper({super.key, required this.builder, this.listener});
 
   /// The builder that will wrap the material app.
-  final Widget Function(BuildContext context, Locale currentLocale) builder;
+  final BlocWidgetBuilder<Locale> builder;
+
+  /// The listener that will wrap the material app.
+  final BlocWidgetListener<Locale>? listener;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +20,10 @@ class LocalizationWrapper extends StatelessWidget {
       create: (context) => getIt<LocalizationCubit>(),
       child: Builder(
         builder: (context) {
-          return BlocBuilder<LocalizationCubit, Locale>(builder: builder);
+          return BlocConsumer<LocalizationCubit, Locale>(
+            builder: builder,
+            listener: listener ?? (context, state) {},
+          );
         },
       ),
     );
